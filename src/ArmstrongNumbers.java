de2ece438c5940882817b1af308b8b8338177b92
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-26 20:51:09 +0530

    Add. Included the Logic For Finding Armstrong Numbers

--- src/ArmstrongNumbers.java
+++ src/ArmstrongNumbers.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ArmstrongNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number to find Armstrong numbe : ");
+        int inputNumber = scanner.nextInt();
+
+        for (int iterator = 1; iterator < inputNumber; iterator++) {
+            int order = String.valueOf(iterator).length();
+            int temp = iterator;
+            int sum = 0;
+            while (temp > 0) {
+                int lastValue = temp % 10;
+                sum += Math.pow(lastValue, order);
+                temp = temp / 10;
+            }
+            if (sum == iterator)
+                System.out.println(iterator + " : Yes It is a Armstrong no");
+        }
     }
 }
